b3766ce45bcf3bbe464f825d2a0c39f25b896eab
nerd4141 <nerd4141>
2018-11-02 20:11:00 +0000

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pyogp docker container
 
 ````
-docker run -e SL_FIRSTNAME='x' -e SL_SURNAME='resident' -e SL_PASSWORD='x' <container id>
+docker run -e SL_FIRSTNAME='x' -e SL_SURNAME='resident' -e SL_PASSWORD='x' -e SL_REGION='' <container id>
 ````
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/hg/pyogp.apps/pyogp/apps/examples
-sudo -u pyogp python region_connect.py $SL_FIRSTNAME $SL_SURNAME --password "$SL_PASSWORD"
+sudo -u pyogp python region_connect.py $SL_FIRSTNAME $SL_SURNAME --password "$SL_PASSWORD" --region "$SL_REGION"
